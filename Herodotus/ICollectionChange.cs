3acd65aa8532126b308b9beae69589d33bb87216
quanben <quanben@msn.com>
2014-04-02 02:19:44 +1000

    Added ICollectionChange.cs missing from last check-in.

---
+++ Herodotus/ICollectionChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+
+namespace Herodotus
+{
+    public interface ICollectionChange
+    {
+        #region Properties
+
+        object Collection { get; }
+
+        NotifyCollectionChangedAction Action { get; }
+
+        IList OldItems { get; set; }
+
+        IList NewItems { get; set; }
+
+        int NewStartingIndex { get; }
+
+        int OldStartingIndex { get; set; }
+
+        #endregion
+    }
+}
